6395ea5175112d1c54574713c22efa1ba9b43513
Albert Previ <36011199+albertaruberto@users.noreply.github.com>
2019-12-09 21:03:07 +0100

    Add files via upload

---
+++ Cost per km for a Fiat Panda 90'.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+
+#driving the car to work
+kmToWork = 15
+driveAndReturnWork = kmToWork * 2
+
+#others km driving in one week
+others = 30
+
+#Month in one Year
+oneYear = 12
+totalKm = (driveAndReturnWork + others)
+
+#Total avarage km driving in one Year
+totalKmInOneYear = (totalKm * oneYear)
+print(totalKmInOneYear)
+
+#Insurance for Fiat Panda
+insurance = 316
+
+#Auto tax
+autoTax = 112
+
+#total car cost in one Year
+CostPerKm = 0.39 #cost per km for Fiat Panda
+totCarCost = totalKmInOneYear * CostPerKm + insurance + autoTax
+print(totCarCost)
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
